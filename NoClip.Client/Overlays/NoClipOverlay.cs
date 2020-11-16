87d438484522581fdb12caf28e9f20ad06d3284c
CroneFacade <bernhardsson.philip@gmail.com>
2020-11-16 19:11:39 +0100

    Removed unused overlay

--- NoClip.Client/Overlays/NoClipOverlay.cs
+++
@@ -1,9 +0,0 @@
-using NFive.SDK.Client.Interface;
-
-namespace CroneFacade.NoClip.Client.Overlays
-{
-	public class NoClipOverlay : Overlay
-	{
-		public NoClipOverlay(IOverlayManager manager) : base(manager) { }
-	}
-}
